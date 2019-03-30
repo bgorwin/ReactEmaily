6eeeea2cf9906812bf753613a2d1d0ed8c871f99
Brent Gorwin <brent.gorwin@gmail.com>
2019-03-30 08:43:06 -0500

    changed greeting

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
+  res.send({ hi: 'there, we changed something' });
 });
 
 const PORT = process.env.PORT || 5000;
