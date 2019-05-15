9bec084f0cf52568796c846cc6fb8d04d280c9d3
Brent Gorwin <brent.gorwin@gmail.com>
2019-05-15 17:30:55 -0500

    fixed the react server issue, npm module

--- client/src/App.js
+++ client/src/App.js
@@ -9,8 +9,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          
         </p>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
