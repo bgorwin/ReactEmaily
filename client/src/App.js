a7a04ad22083af66bee43ef803cdaaef007c67de
Brent Gorwin <brent.gorwin@gmail.com>
2019-05-22 10:06:25 -0500

    Added a new refactor to the sign in strategy

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        
+        <a href="/auth/google">Click here to sign in with Google</a>
         <a
           className="App-link"
           href="https://reactjs.org"
