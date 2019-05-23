b186e3b94273450a90a2a01bd96dcf8f70babb95
Brent Gorwin <brent.gorwin@gmail.com>
2019-05-23 14:00:12 -0500

    wiring up redux and sign in header funcitonality

---
+++ client/src/components/Header.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+
+class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return 'Still deciding';
+      case false:
+        return 'Im logged out';
+      default:
+        return 'im logged in';
+    }
+  }
+
+  render() {
+    console.log(this.props);
+    return (
+      <nav>
+        <div className="nav-wrapper">
+          <a className="left brand-logo">
+            Emaily
+          </a>
+          <ul className="right">
+            {this.renderContent()}
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
+
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps) (Header);
