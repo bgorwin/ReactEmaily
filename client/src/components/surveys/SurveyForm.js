e3140c5d6a540a98ba9c129507b1a0111c1256bb
Brent Gorwin <brent.gorwin@gmail.com>
2019-07-05 17:24:15 -0500

    Added the files for the survey form & survey form review page. Handled this with state and props instead of using Redux store

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,20 +1,59 @@
 //SurveyForm shows a form fro a user to add input
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
+import { Link } from 'react-router-dom';
+import SurveyField from './SurveyField';
+import validateEmails from '../../utils/validateEmails';
+
+const FIELDS = [
+  { label: 'Survey Title', name: 'title' },
+  { label: 'Subject Line', name: 'subject' },
+  { label: 'Email Body', name: 'body' },
+  { label: 'Recipient List', name: 'emails' }
+];
 
 class SurveyForm extends Component {
+  renderFields(){
+    return _.map(FIELDS, ({ label, name }) => {
+      return (
+        <Field key={name} component={SurveyField} type="text" label={label} name={name} />
+      );
+    });
+  }
+
   render() {
     return (
       <div>
-        <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
-          <Field type="text" name="surveyTitle" component="input" />
-          <button type="submit">Submit</button>
+        <form onSubmit={this.props.handleSubmit(this.props.onSurveySubmit)}>
+          {this.renderFields()}
+
+          <Link to="/surveys" className="red btn-flat white-text">
+          Cancel
+          </Link>
+
+          <button className="teal btn-flat right white-text" type="submit">Next <i className="material-icons right">done</i></button>
         </form>
       </div>
     );
   }
 }
 
+function validate(values){
+  const errors = {};
+
+  errors.emails = validateEmails(values.emails || '');
+
+  _.each(FIELDS, ({ name }) => {
+    if(!values[name]){
+      errors[name] = 'You must provide a value';
+    }
+  });
+
+  return errors;
+}
+
 export default reduxForm({
+  validate,
   form: 'surveyForm'
 })(SurveyForm);
