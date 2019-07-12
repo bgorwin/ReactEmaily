3db6837614229325384e1d968bbfd20e23e09085
Brent Gorwin <brent.gorwin@gmail.com>
2019-07-12 15:43:41 -0500

    lodash package upgrade

--- services/emailTemplates/surveyTemplate.js
+++ services/emailTemplates/surveyTemplate.js
@@ -1,19 +1,18 @@
 const keys = require('../../config/keys');
 
-module.export = survey => {
+module.exports = survey => {
   return `
     <html>
       <body>
         <div style="text-align: center;">
           <h3>I'd like your input!</h3>
-          <p>Please answer the following questions:</p>
+          <p>Please answer the following question:</p>
           <p>${survey.body}</p>
-
           <div>
-            <a href="${keys.redirectDomain}/api/surveys/thanks">Yes</a>
+            <a href="${keys.redirectDomain}/api/surveys/${survey.id}/yes">Yes</a>
           </div>
           <div>
-            <a href="${keys.redirectDomain}/api/surveys/thanks">No</a>
+            <a href="${keys.redirectDomain}/api/surveys/${survey.id}/no">No</a>
           </div>
         </div>
       </body>
