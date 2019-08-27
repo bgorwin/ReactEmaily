72cbc52b06ff14f266488d99fc1ccb6bc63b31f9
Brent Gorwin <brent.gorwin@gmail.com>
2019-08-26 19:54:59 -0500

    Fixed the survey form field styling

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -18,7 +18,7 @@ class SurveyForm extends Component {
 
   render() {
     return (
-      <div>
+      <div className="container">
         <form onSubmit={this.props.handleSubmit(this.props.onSurveySubmit)}>
           {this.renderFields()}
 
--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -19,7 +19,7 @@ const SurveyFormReview = ({ onCancel, formValues, submitSurvey, history }) => {
   });
 
   return (
-    <div>
+    <div className="container">
       <h5>Please confirm your entries</h5>
       {reviewFields}
       <button
